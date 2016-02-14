ea3855564950b6dc77e579a2459905715f5269d3
jonathan <jonathan@adeogroup.co.uk>
2016-02-14 21:14:22 +0000

    modifications

--- specified-url-tests/jonathans_scanner.py
+++ specified-url-tests/jonathans_scanner.py
@@ -2,11 +2,11 @@
 import sys
 import os
 import subprocess
+import signal
 
 class jonathans_scanner:
 
-	def __init__(self, utility_name):
-		
+	def __init__(self, utility_name):	
 		self.ultilty_name = utility_name
 		self.pwd = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))	
 		self.reports_path = os.path.join(self.pwd, 'reports')
@@ -17,13 +17,11 @@ class jonathans_scanner:
 		self.urls = [line.rstrip('\n') for line in open(os.path.join(self.pwd,'urllist.txt'))]
 		self.authenticated = self.authenticate()
 		self.session_id = open(os.path.join(self.pwd,'session.txt'))
-		
 
 	def runThread(self, cmd, logfile):
-	    thread = subprocess.Popen(cmd, shell=True, universal_newlines=True, stdout=logfile)
+	    thread = subprocess.Popen(cmd, shell=False, universal_newlines=True, stdout=logfile)
 	    thread.wait()
-	    return thread
-
+	    
 	def logResult(self, cmd, logname):
 	    with open(logname, "a") as log:
 	        print >> log, "loading scanner: " + cmd
@@ -32,7 +30,11 @@ class jonathans_scanner:
 
 	def launchZaproxy(self):
 		cmd = '/usr/share/zaproxy/zap.sh -daemon -dir ' + self.reports + ' -newsession ' + self.ultilty_name
-		thread = subprocess.Popen(cmd, shell=False, universal_newlines=True)
+		zaproxy_instance = subprocess.Popen(cmd, shell=False, universal_newlines=True, preexec_fn=os.setsid)
+		return zaproxy_instance
+
+	def closeZaproxy(self, zaproxy_instance):
+		os.killpg(os.getpgid(zaproxy_instance.pid), signal.SIGTERM)
 		
 	def authenticate(self):
 		# we cant cleanup after, due to the way subprocess forks, so we will do a cleanup before
@@ -44,8 +46,10 @@ class jonathans_scanner:
 
 	def begin(self, cmd):
 		with open(self.logfile, "w") as log:
+			zaproxy_instance = self.launchZaproxy
 			print >> log, "Scan results"
 			self.runThread(cmd, log)
+			self.closeZaproxy(zaproxy_instance)
 
 
 
