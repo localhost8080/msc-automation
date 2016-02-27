ebf36121098442dec9d4b880a3aba0f4eb8046e6
jonathan <jonathan@adeogroup.co.uk>
2016-02-27 18:40:55 +0000

    modifications

--- specified-url-tests/jonathans_scanner.py
+++ specified-url-tests/jonathans_scanner.py
@@ -15,21 +15,37 @@ class jonathans_scanner:
 		self.reports_path = os.path.join(self.pwd, 'reports')
 		self.reports = os.path.join(self.reports_path, self.ultilty_name)
 		
-		# need these for dvwa and for wordpress
+		# need these for dvwa
 		self.dvwa_cookiefile = os.path.join(self.pwd,'cookie.txt')
 		self.dvwa_jsoncookiefile = os.path.join(self.pwd,'cookie.json')
-		self.dvwa_urls = [line.rstrip('\n') for line in open(os.path.join(self.pwd,'dvwa-urls.txt'))]
+		self.dvwa_urls = [line.rstrip('\n') for line in open(os.path.join(self.pwd,'dvwa_urls.txt'))]
 
+		# need these for wordpress
+		self.wp15_cookiefile = os.path.join(self.pwd,'wp15_cookie.txt')
+		self.wp15_jsoncookiefile = os.path.join(self.pwd,'wp15_cookie.json')
+		self.wp15_urls = [line.rstrip('\n') for line in open(os.path.join(self.pwd,'wp15_urls.txt'))]
 
-		self.wp15_cookiefile = os.path.join(self.pwd,'wordpress1-5cookie.txt')
-		self.wp15_jsoncookiefile = os.path.join(self.pwd,'wordpress1-5cookie.json')
-		self.wp15_urls = [line.rstrip('\n') for line in open(os.path.join(self.pwd,'wordpress1-5-urls.txt'))]
+		# need these for drupal
+		self.drupal_cookiefile = os.path.join(self.pwd,'drupal_cookie.txt')
+		self.drupal_jsoncookiefile = os.path.join(self.pwd,'drupal_cookie.json')
+		self.drupal_urls = [line.rstrip('\n') for line in open(os.path.join(self.pwd,'drupal_urls.txt'))]
 
+		# need these for joomla
+		self.joomla_cookiefile = os.path.join(self.pwd,'joomla_cookie.txt')
+		self.joomla_jsoncookiefile = os.path.join(self.pwd,'joomla_cookie.json')
+		self.joomla_urls = [line.rstrip('\n') for line in open(os.path.join(self.pwd,'joomla_urls.txt'))]
+
+		#auth with them all for now
 		self.authenticated = self.authenticate()
 		dvwa_session_content = [line.rstrip('\n') for line in open(os.path.join(self.pwd,'session.txt'))]
-		wp15_session_content = [line.rstrip('\n') for line in open(os.path.join(self.pwd,'wordpress1-5session.txt'))]
+		wp15_session_content = [line.rstrip('\n') for line in open(os.path.join(self.pwd,'wp15_session.txt'))]
+		wp15_session_content = [line.rstrip('\n') for line in open(os.path.join(self.pwd,'drupal_session.txt'))]
+		wp15_session_content = [line.rstrip('\n') for line in open(os.path.join(self.pwd,'joomla_session.txt'))]
 		self.dvwa_session_id = ' '.join(dvwa_session_content)
 		self.wp15_session_id = ' '.join(wp15_session_content)
+		self.drupal_session_id = ' '.join(drupal_session_content)
+		self.joomla_session_id = ' '.join(joomla_session_content)
+		
 		self.logfile = ''
 		self.base_url = ''
 		self.fixed_base_url = ''
@@ -39,27 +55,24 @@ class jonathans_scanner:
 		print "\n"+cmd+"\n\n\n"
 		thread = subprocess.Popen("exec " + cmd , shell=True, stdout=logfile)
 		thread.wait()
-	    
-	def launchZaproxy(self):
-		zap = ZAPv2()
-
-		#cmd = '/usr/share/zaproxy/zap.sh -daemon -dir ' + self.reports + ' -newsession ' + self.ultilty_name
-		#zaproxy_instance = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid) 
-		#return zaproxy_instance
-
-	def closeZaproxy(self, zaproxy_instance):
-		os.killpg(os.getpgid(zaproxy_instance.pid), signal.SIGTERM)
 		
 	def authenticate(self):
 		# we cant cleanup after, due to the way subprocess forks, so we will do a cleanup before
 		cleanup = subprocess.Popen(os.path.join(self.pwd,'cleanup'), shell=False)
 		cleanup.wait()
 		
-		authenticate = subprocess.Popen(os.path.join(self.pwd,'authenticate-dvwa'), shell=False)
+		authenticate = subprocess.Popen(os.path.join(self.pwd,'authenticate_dvwa'), shell=False)
 		authenticate.wait()
 
-		authenticate = subprocess.Popen(os.path.join(self.pwd,'authenticate-wordpress1.5'), shell=False)
+		authenticate = subprocess.Popen(os.path.join(self.pwd,'authenticate_wp15'), shell=False)
 		authenticate.wait()
+
+		authenticate = subprocess.Popen(os.path.join(self.pwd,'authenticate_drupal'), shell=False)
+		authenticate.wait()
+
+		authenticate = subprocess.Popen(os.path.join(self.pwd,'authenticate_joomla'), shell=False)
+		authenticate.wait()
+
 
 	def setUrls(self, url):
 		self.base_url = url[:url.find('?')]
